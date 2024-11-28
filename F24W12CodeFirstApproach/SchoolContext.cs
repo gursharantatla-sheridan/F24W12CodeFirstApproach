24a7cbf19a7b7c1a156e5dceb85eedf62d71550c
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-11-28 13:59:59 -0500

    context

---
+++ F24W12CodeFirstApproach/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24W12CodeFirstApproach
+{
+    // context class
+    // a class that inherits from built-in DbContext class
+    public class SchoolContext : DbContext
+    {
+        // entity sets - (required)
+        // define entity properties of type DbSet<>
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+
+
+        // database connection - (required)
+        // define the connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=SchoolCF; Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+
+
+        // data seeding - {optional)
+
+    }
+}
